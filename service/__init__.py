e45e96b80f46dc0a7bd41652011e6e4904e384f2
neo1218 <neo1218@yeah.net>
2017-04-29 14:38:14 +0800

    updaye

--- service/__init__.py
+++ service/__init__.py
@@ -3,12 +3,12 @@ import asyncio
 from aiohttp import web
 from aiohttp_session import setup, get_session, session_middleware
 from aiohttp_session.cookie_storage import EncryptedCookieStorage
-from cryptography import fernet
+# from cryptography import fernet
 
 def create_app():
     app = web.Application()
-    fernet_key = fernet.Fernet.generate_key()
-    secret_key = base64.urlsafe_b64decode(fernet_key)
+    # fernet_key = fernet.Fernet.generate_key()
+    secret_key = base64.urlsafe_b64decode("$")
 
     setup(app, EncryptedCookieStorage(secret_key))
 
