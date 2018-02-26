e645fc8ef1255acd8dc4d5c784a2dd016a066751
Humbertzhang <504490160@qq.com>
2018-02-26 10:36:59 +0800

    update:change the mongo db name

--- service/database.py
+++ service/database.py
@@ -6,8 +6,8 @@ MONGOPORT = int(os.getenv('MONGOPORT') or '27017')
 
 async def setup_db():
     client = AsyncIOMotorClient(MONGOHOST, MONGOPORT)
-    tabledb = client['tabledb']  # 存储信息门户课表
-    userdb = client['userdb']    # 存储自定义课表
+    tabledb = client['tabledb_2018_1']  # 存储信息门户课表
+    userdb = client['userdb_2018_1']    # 存储自定义课表
     tablecol = tabledb['tables'] # 信息门户课表集合
     usercol = userdb['users']    # 自定义课表集合
     return tabledb, userdb
