2a6cea04d874911ea5e81fdb7121d9241362e84e
yuyilei <yyl1352091742@gmail.com>
2018-04-28 22:04:40 +0800

    fix return format bug

--- service/szkc_spider.py
+++ service/szkc_spider.py
@@ -13,8 +13,8 @@ headers = {
     "Content-Type":"application/x-www-form-urlencoded;charset=UTF-8",
 }
 
-sid = os.getenv('ADMIN_SID')
-pwd = os.getenv('ADMIN_PWD')
+sid = os.getenv('ADMIN_SID') or 'muxi'
+pwd = os.getenv('ADMIN_PWD') or 'ihdmx123'
 
 async def get_szkc_table(xnm, xqm, s):
     cookies = await login_szkc(sid, pwd)
@@ -47,25 +47,82 @@ async def get_szkc_table(xnm, xqm, s):
                 return res
             #print(json_data['items'])
             for each in json_data['items'] :
+                #print(each)
+                t_info = each['sksj']                                # 星期一第1-2节{6周};星期一第1-2节{7周};星期一第1-2节{8周};星期一第1-2节{9周}
+                #print(t_info)
+                oneday_info = t_info.split(';')
+                #print(oneday_info)
+                for oneday in oneday_info :                          # 星期一第1-2节{6周}
+                    #print(oneday)
+                    whichday = oneday[:3]
+                    k_index = oneday.find("{")
+                    zhou_index = oneday.find("}")
+                    _weeks = oneday[k_index + 1:zhou_index]           # 11周，10-13周，或10-5周（双）
+                    #print(_weeks)
+                    week_list = getweek(_weeks)                       # ['10', '11', '12', '13'] ['11-12']
+                    #print(week_list)
+
+                    di_index = oneday.find("第")
+                    jie_index = oneday.find("节")
+                    times = oneday[di_index + 1:jie_index]  # 1-4,7-10
+                    # print(times)
+                    times_ = times.split(',')                         # ['1-4', '7-10']
+                    # print(oneday[:3],times_)
+
+                    for times in times_ :                             # ['1-2','4-5']
+                        times = times.split('-')                      # ['1','2']
+                        start = int(times[0])
+                        during = int(times[1]) - int(times[0]) + 1
+                        #print(start,during)                          # 1, 2
+                        one = {
+                            'course': each['kcmc'],
+                            'teacher': each['jsxm'],
+                            'place': each['jxdd'],
+                            'day': whichday,
+                            'start': start,
+                            'during': during,
+                            "weeks": ",".join(week_list),
+                            'remind': False,
+                        }
+                        res.append(one)
+                        print(one)
+
+                """
                 new_data = each['sksj'].split(';')
                 #print(new_data)
                 #print(each['sksj'])
-                week_list = []
+                #week_list = []
+                start_during = []
+                res_week =[]
                 for each_time in new_data :
                     di_index = each_time.find("第")
                     jie_index = each_time.find("节")
                     times = each_time[di_index+1:jie_index]
-                    times = times.split('-')
-                    start = int(times[0])
-                    during  = int(times[1]) - int(times[0]) + 1
-
+                    more_time = times.split(',')
+                    res_time = []
+                    print(more_time)
+                    #start_during = []
+                    for each_t in more_time:
+                        res_time.append(each_t.split('-'))
+                    for times in res_time:
+                        #times = times.split('-')
+                        #print(times)
+                        tmp = []
+                        start = int(times[0])
+                        during  = int(times[1]) - int(times[0]) + 1
+                        tmp.append(start)
+                        tmp.append(during)
+                        start_during.append(tmp)
+                    #print(each_time)
                     k_index = each_time.find("{")
                     zhou_index = each_time.find("}")
                     _weeks = each_time[k_index+1:zhou_index]
-                    #week_list = []
+                    week_list = []
                     #print(_weeks)
+
                     if ',' in _weeks :
                         _weeks = _weeks.split(',')
+                        print(_weeks)
                         for item in _weeks :
                             if '-' in item:
                                 if "周(双)" in item:
@@ -102,18 +159,23 @@ async def get_szkc_table(xnm, xqm, s):
                             week_list.extend(_week_list)
                         else :
                             week_list.append(_weeks[:-1])
-                one = {
-					            'course': each['kcmc'],
-					            'teacher': each['jsxm'],
-					            'place': each['jxdd'],
-					            'day': each['sksj'][:3],
-					            'start' : start,
-					            'during' : during,
-					            "weeks" : ",".join(week_list),
-					            'remind' : False,
-				            }
-                res.append(one)
-                print(one)
+
+                for each_start in start_during :
+                    #print(each_start)
+                    pass
+                    one = {
+                                    'course': each['kcmc'],
+                                    'teacher': each['jsxm'],
+                                    'place': each['jxdd'],
+                                    'day': each['sksj'][:3],
+                                    'start' : start,
+                                    'during' : during,
+                                    "weeks" : ",".join(week_list),
+                                    'remind' : False,
+                                }
+                    res.append(one)
+                    print(one)
+                    """
             return res
         return None
 
@@ -156,8 +218,56 @@ async def login_szkc(sid, pwd):
                                 return {"msg":msg}
 
 
+def getweek(_weeks) :
+    """
+    11周，10-13周，或10-5周（双）
+    :param _weeks:
+    :return:
+    """
+    week_list = []
+    if ',' in _weeks:
+        _weeks = _weeks.split(',')
+        print(_weeks)
+        for item in _weeks:
+            if '-' in item:
+                if "周(双)" in item:
+                    tmp_week = item.split('-')
+                    _start = int(tmp_week[0])
+                    _end_list = tmp_week[1][:-1].split("周")
+                    _end = int(_end_list[0])
+                    _week_list = [str(i) for i in range(_start, _end + 1) if i % 2 == 0]
+                    week_list.extend(_week_list)
+                elif "周(单)" in item:
+                    tmp_week = item.split('-')
+                    _start = int(tmp_week[0])
+                    _end_list = tmp_week[1][:-1].split("周")
+                    _end = int(_end_list[0])
+                    _week_list = [str(i) for i in range(_start, _end + 1) if i % 2 == 1]
+                    week_list.extend(_week_list)
+                else:
+                    tmp_week = item.split('-')
+                    # print(tmp_week)
+                    _start = int(tmp_week[0])
+                    _end = int(tmp_week[1][:-1])
+                    # print(_start,_end)
+                    _week_list = [str(i) for i in range(_start, _end + 1)]
+                    week_list.extend(_week_list)
+                # print(week_list)
+            else:
+                week_list.append(item[:-1])
+    else:
+        if '-' in _weeks:
+            tmp_week = _weeks.split('-')
+            _start = int(tmp_week[0])
+            _end = int(tmp_week[1][:-1])
+            _week_list = [str(i) for i in range(_start, _end + 1)]
+            week_list.extend(_week_list)
+        else:
+            week_list.append(_weeks[:-1])
+    return week_list
+
 if __name__ == '__main__' :
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(get_szkc_table(2017,12,2017211077))
+    loop.run_until_complete(get_szkc_table(2017,12,2017211762))
     loop.close()
 
