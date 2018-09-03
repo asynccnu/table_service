eabffca15ba0b9bdc9e369ee6918ac5b9882d6ea
Humbertzhang <504490160@qq.com>
2018-09-03 10:07:47 +0800

    Update api.py

--- service/api.py
+++ service/api.py
@@ -108,7 +108,7 @@ async def add_table_api(request, sid, ip):
         await userdb['users'].insert_one({'sid': sid, 'table': user_table})
     return web.json_response({'id': max_id+1})
 
-@require_sid
+@require_info_login
 async def del_table_api(request,s, sid, ip):
     """
     删除课程API/(可以)删除信息门户课程
@@ -151,7 +151,7 @@ async def del_table_api(request,s, sid, ip):
         content_type='application/json', status=404
     )
 
-@require_sid
+@require_info_login
 async def update_table_api(request, s, sid, ip):
     """
     更新课程API/自定义/信息门户课程
