126a5565bc69fdb4babbc4eb37a6a1c78a7e0bf9
yuyilei <yyl1352091742@gmail.com>
2018-09-03 15:28:13 +0800

    update for szkc

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 from service import app, web, loop
 from service.database import setup_db
-from service.api import api
+from service.apis import api
 
 tabledb, userdb = loop.run_until_complete(setup_db())
 api['tabledb'] = tabledb
