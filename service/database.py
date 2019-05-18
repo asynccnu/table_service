cbd4fb891912cb8acfae1b7414823394ddfed329
Darren <darren@Darren-MBP.lan>
2019-05-19 01:57:35 +0800

    fix: add mongodb auth

--- service/database.py
+++ service/database.py
@@ -3,11 +3,15 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 MONGOHOST = os.getenv('MONGOHOST') or 'test_mongo3'
 MONGOPORT = int(os.getenv('MONGOPORT') or '27017')
+MONGODB_USERNAME = os.getenv('MONGODB_USERNAME') or "muxi"
+MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD') or "nopassword"
 TABLEDB = os.getenv('TABLEDB') or 'tabledb'
 USERDB = os.getenv('USERDB') or 'userdb'
 
 async def setup_db():
-    client = AsyncIOMotorClient(MONGOHOST, MONGOPORT)
+    # client = AsyncIOMotorClient(MONGODB_HOST, MONGODB_PORT)
+    mongo_uri = "mongodb://{}:{}@{}:{}".format(MONGODB_USERNAME, MONGODB_PASSWORD, MONGODB_HOST, MONGODB_PORT)
+    client = AsyncIOMotorClient(mongo_uri)
     tabledb = client[TABLEDB]  # 存储信息门户课表
     userdb = client[USERDB]    # 存储自定义课表
     tablecol = tabledb['tables'] # 信息门户课表集合
